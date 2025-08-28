812342691bf1c0bda56e3397e6c700325d197013
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-08-28 13:15:03 +0530

    day1.c

--- day1.c
+++ day1.c
@@ -1,15 +1,12 @@
 //write a program to input two numbers and display their sum.
 
-#include<stdio.h>
-int main() {
-    int a, b;
-    printf("Enter a: ");
-    scanf("%d", &a);
+#include <stdio.h>
 
-    printf("Enter b: ");
-    scanf("%d", &b);
-
-    prinf(" %d", a+b);
+int main(){
+    int a,b;
+    printf(" enter two numbers : \n");
+    scanf("%d %d", &a ,&b);
+    printf("sum of two numebers id : %d \n", a+b);
 
     return 0;
 }
