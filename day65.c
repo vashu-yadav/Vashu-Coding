87b5015cbcffec98b0a8d7d491d8f86b1266c362
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-11-08 09:47:12 +0530

    Create day65.c

---
+++ day65.c
@@ -0,0 +1,13 @@
+//practice questions
+
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d", &num);
+    for(int i = 1; i <= num; i++) {
+        if(num % i == 0)
+            printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
