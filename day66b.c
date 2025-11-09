0ec26a061b8d10218f80e2d56a23a011ff1dc5fa
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-11-09 11:42:29 +0530

    Create day66b.c

---
+++ day66b.c
@@ -0,0 +1,14 @@
+//practice questions
+
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0;
+    scanf("%d", &n);
+    while(n > 0) {
+        rev = rev * 10 + n % 10;
+        n = n / 10;
+    }
+    printf("%d\n", rev);
+    return 0;
+}
