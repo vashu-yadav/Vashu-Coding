63ef6008b97a0248635b02e948d0820ea2305d6e
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-11-12 21:13:47 +0530

    Create day73.c

---
+++ day73.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c)
+        printf("Equilateral\n");
+    else if (a == b || b == c || a == c)
+        printf("Isosceles\n");
+    else
+        printf("Scalene\n");
+
+    return 0;
+}
