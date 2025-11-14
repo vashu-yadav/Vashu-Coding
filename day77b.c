bc4dcd52c3344ec7dba5bda149e504cbe01c6dc7
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-11-14 22:47:53 +0530

    Create day77b.c

---
+++ day77b.c
@@ -0,0 +1,10 @@
+
+void toUppercase(char *str) {
+    int i = 0;
+    while(str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+}
