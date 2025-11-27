bc745fd27ec1316cc0d89dda6c0c8b0f42128dae
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-11-27 21:06:53 +0530

    Create day100.c

---
+++ day100.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+enum Day { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY };   // enum gives named int constants [web:23][web:20]
+
+int main(void)
+{
+    enum Day d;
+
+    for (d = SUNDAY; d <= SATURDAY; d++) {
+        switch (d) {
+        case SUNDAY:    printf("SUNDAY = %d\n", d); break;
+        case MONDAY:    printf("MONDAY = %d\n", d); break;
+        case TUESDAY:   printf("TUESDAY = %d\n", d); break;
+        case WEDNESDAY: printf("WEDNESDAY = %d\n", d); break;
+        case THURSDAY:  printf("THURSDAY = %d\n", d); break;
+        case FRIDAY:    printf("FRIDAY = %d\n", d); break;
+        case SATURDAY:  printf("SATURDAY = %d\n", d); break;
+        }
+    }
+
+    return 0;
+}
