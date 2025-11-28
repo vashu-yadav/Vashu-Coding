a0ead59824b1f3fa2aa2b23b2edc024e405452d3
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-11-28 21:20:59 +0530

    Create day102.c

---
+++ day102.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+enum Numbers {
+    TEN = 10,
+    ELEVEN,
+    TWELVE,
+    THIRTEEN
+};
+
+int main() {
+    printf("TEN = %d\n", TEN);
+    printf("ELEVEN = %d\n", ELEVEN);
+    printf("TWELVE = %d\n", TWELVE);
+    printf("THIRTEEN = %d\n", THIRTEEN);
+    return 0;
+}
