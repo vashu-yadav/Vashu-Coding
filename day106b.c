1c970ccd5f833c7261fa70abd07559af6c14a9c6
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-11-30 10:36:52 +0530

    Create day106b.c

---
+++ day106b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int incr (int i)
+{
+   static int count = 0;
+   count = count + i;
+   return (count);
+}
+main ()
+{
+   int i,j;
+   for (i = 0; i <=4; i++)
+      j = incr(i);
+}
