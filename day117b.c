eed7e08bf7e09b91b1a16e126e20ea4ad10bbd1a
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-12-07 10:50:57 +0530

    Create day117b.c

---
+++ day117b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+
+    fp = fopen("output.txt", "r");   // open existing file
+    if (fp == NULL) {
+        printf("File not found!\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        putchar(ch);
+    }
+
+    fclose(fp);
+    return 0;
+}
