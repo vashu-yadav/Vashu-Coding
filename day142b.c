c3e300409c832ae9aae86c92b717c7e34ef88509
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2025-12-19 17:54:23 +0530

    Create day142b.c

---
+++ day142b.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void reverse(int arr[], int n) {
+    int i = 0, j = n - 1, temp;
+    while (i < j) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+int main(void) {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    reverse(arr, n);
+
+    printf("Reversed array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
