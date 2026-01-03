33dfbb80de1268a4bb632fc51578a5c7c150f940
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-01-03 13:06:28 +0530

    Create day170b.c

---
+++ day170b.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secret, guess, tries = 0;
+    srand(time(NULL));
+    secret = rand() % 100 + 1;   // 1–100
+
+    printf("Guess the number (1-100):\n");
+    do {
+        printf("Enter guess: ");
+        scanf("%d", &guess);
+        tries++;
+
+        if (guess > secret)
+            printf("Too high!\n");
+        else if (guess < secret)
+            printf("Too low!\n");
+        else
+            printf("Correct in %d tries!\n", tries);
+    } while (guess != secret);
+
+    return 0;
+}
