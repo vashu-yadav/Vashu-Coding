b962d851ecd0111df332593a52c2a8d6dd8d70bf
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-01-12 00:54:21 +0530

    c

---
+++ day187.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int b[3][2] = {{7, 8}, {9, 10}, {11, 12}};
+    int result[2][2] = {0};
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 3; k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    printf("Result:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
